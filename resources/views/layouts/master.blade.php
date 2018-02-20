18f55a023e78b81e6f3a313bce27b4cba42675bf
travier <moorlagt@gmail.com>
2018-02-20 14:46:55 -0500

    updated favicon

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,6 +6,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link href="{{ URL::asset('css/app.css') }}" rel="stylesheet" >
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=BGAqg9LG5E">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=BGAqg9LG5E">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=BGAqg9LG5E">
+    <link rel="manifest" href="/site.webmanifest?v=BGAqg9LG5E">
+    <link rel="shortcut icon" href="/favicon.ico?v=BGAqg9LG5E">
+    <meta name="apple-mobile-web-app-title" content="Downstream">
+    <meta name="application-name" content="Downstream">
+    <meta name="msapplication-TileColor" content="#026876">
+    <meta name="theme-color" content="#026876">
     @if(!Auth::guest())
     <meta name="xyz-token" content="{{ auth()->user()->api_token }}">
     @endif
@@ -15,7 +24,7 @@
 
   <body>
     <div id="app">
-      <nav class="main-navbar navbar-expand-md navbar navbar-dark navbar-deco" style="background-color: #117F90;">
+      <nav class="main-navbar navbar-expand-md navbar navbar-dark navbar-deco" style="background-color: #038294;">
         <div class="container-fluid"> 
           <a id="brand" class="navbar-brand" style="font-size:1.6em;" href="{{ url('/') }}">Downstream</a>
           <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
