02c8570485c663938c1963fb80d16f5671f0bf4f
travier <moorlagt@gmail.com>
2018-03-26 23:23:06 -0400

    fixed spelling

--- resources/views/landing/main.blade.php
+++ resources/views/landing/main.blade.php
@@ -4,7 +4,7 @@
   <div class="row justify-content-md-center">
   	<div class="col-lg-12">
     	<h1 style="text-align: center;" >Discover <img src="/open-iconic-master/svg/chevron-right.svg"> Collect <img src="/open-iconic-master/svg/chevron-right.svg"> Play</h1>
-    	<p style="text-align: center; font-size: 1.2rem;">A unique music service designed to help you discover and collect what ever type of music you enjoy listening to.</p>
+    	<p style="text-align: center; font-size: 1.2rem;">A unique music service designed to help you discover and collect whatever type of music you enjoy listening to.</p>
 	</div>
   </div>
 
