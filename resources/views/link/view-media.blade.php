ab84da093310beb839f4fc2bdc91e10282dd97ca
travier <moorlagt@gmail.com>
2018-04-12 13:54:23 -0400

    fixed meta link

--- resources/views/link/view-media.blade.php
+++ resources/views/link/view-media.blade.php
@@ -9,7 +9,7 @@
       @if(env("FB_APP_ID"))
         <meta property="fb:app_id"  content="{{ env('FB_APP_ID') }}" />
       @endif
-    	<meta property="og:url"   content="https://down-stream.org/media/{{$media->index}}" />
+    	<meta property="og:url"   content="{{env('APP_LINK_URL') . $media->index}}" />
   	  <meta property="og:type"  content="music.song" />
   	  <meta property="og:title" content="{{$media->meta->title }}" />
   	  <meta property="og:image" content="{{$media->meta->thumbnail }}" />
