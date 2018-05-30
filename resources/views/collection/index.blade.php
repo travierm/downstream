bbff41a69f8ac666cee088503cdc31afb3a4fcab
Liquid Satoshi <Travier@users.noreply.github.com>
2018-05-30 14:36:04 -0400

    testing video lengths

--- resources/views/collection/index.blade.php
+++ resources/views/collection/index.blade.php
@@ -4,7 +4,7 @@
   <div class="row">
     @if(@$videos)
       @foreach($videos as $video)
-      <div class="col-lg-3">
+      <div class="col-lg-3 col-md-6 col-sm-12">
         <youtube-player-card id="{{$video->id}}" collected="{{$video->collected}}" title="{{$video->title}}"  vid="{{$video->vid}}"></youtube-player-card>
       </div>
       @endforeach
