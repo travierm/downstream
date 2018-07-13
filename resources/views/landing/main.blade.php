4a3d9d55cc0d9ff46d73f1eb793cb0eddda33ec4
travier <moorlagt@gmail.com>
2018-07-12 22:07:43 -0400

    updated landing page

--- resources/views/landing/main.blade.php
+++ resources/views/landing/main.blade.php
@@ -1,12 +1,14 @@
 @extends('layouts.master')
 @section('content')
 <div class="container pushFromTop">
-  <div class="row justify-content-md-center">
-  	<div class="col-lg-12">
-    	<h1 style="text-align: center;" >Discover <img src="/open-iconic-master/svg/chevron-right.svg"> Collect <img src="/open-iconic-master/svg/chevron-right.svg"> Play</h1>
-    	<p style="text-align: center; font-size: 1.2rem;">A unique music service designed to help you discover and collect whatever type of music you enjoy listening to.</p>
-	  </div>
-  </div>
+  <section class="jumbotron text-center mb-4">
+    <div class="row justify-content-md-center">
+      <div class="col-lg-12">
+        <h1 class="jumbotron-heading" style="text-align: center;" >Discover <img src="/open-iconic-master/svg/chevron-right.svg"> Collect <img src="/open-iconic-master/svg/chevron-right.svg"> Play</h1>
+        <p style="text-align: center; font-size: 1.2rem;">A unique music service designed to help you discover and collect whatever type of music you enjoy listening to.</p>
+      </div>
+    </div>
+  </section>
 
   @if(Auth::guest())
   <div class="row justify-content-md-center">
@@ -19,7 +21,7 @@
   @if(count($toplist) >= 2)
     <div class="row">
     @foreach($toplist as $item)
-      <div class="col-sm-12 col-md-6 col-lg-6">
+      <div class="col-md-4">
         <video-player-card collected="{{ $item->didCollect }}" spotify-id="{{ $item->source_id }}" vid="{{ $item->index }}" title="{{ $item->title }}" thumbnail="{{ $item->thumbnail }}"></video-player-card>
       </div>
     @endforeach
@@ -27,7 +29,7 @@
   @elseif(count($videos) >= 2)
     <div class="row" style="margin-top: 15px;">
     	@foreach($videos as $media)
-    	<div class="col-sm-12 col-md-6 col-lg-6">
+    	<div class="col-md-4">
         <video-player-card :media="{{ $media }}"></video-player-card>
       </div>
       @endforeach
