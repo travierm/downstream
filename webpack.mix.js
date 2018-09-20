a496efa68dff0c8bbbc928ed2e76929b4e2f2ea2
travier <moorlagt@gmail.com>
2018-09-19 22:06:52 -0400

    laravel 5.6 -> 5.7 upgrade

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,8 +12,8 @@ let mix = require('laravel-mix');
  */
 
 mix
-  .js('resources/assets/js/app.js', 'public/js/app.js')
-  .sass('resources/assets/sass/app.scss', 'public/css')
+  .js('resources/js/app.js', 'public/js/app.js')
+  .sass('resources/sass/app.scss', 'public/css')
 
 if (mix.inProduction()) {
   mix.version();
