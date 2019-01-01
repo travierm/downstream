10009411c295e86e72677954b0ea7e2b4e90fdae
travier <moorlagt@gmail.com>
2018-12-31 23:56:06 -0500

    Better info

--- resources/views/user/spotify-import.blade.php
+++ resources/views/user/spotify-import.blade.php
@@ -6,8 +6,15 @@
         <h2>Spotify Import Setup</h2>
     </div>
 
+
     <div class="row mt-3">
         <div class="col-lg-3">
+            @if($success)
+            <div class="alert alert-success" role="alert">
+                Your Spotify Account is connected with Downstream!
+            </div>
+            @endif
+
             <spotify-authorize-btn @if(@$authorized) authorized="true" @endif></spotify-authorize-btn>
         </div>
     </div>
