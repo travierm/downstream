43470a5887cd3d87b03c7c086b7a14d58eb2a4f1
travier <moorlagt@gmail.com>
2019-02-11 21:03:02 -0500

    Redirect to /collection on login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/all';
+    protected $redirectTo = '/collection';
 
     /**
      * Create a new controller instance.
