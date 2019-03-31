bae29e4a193b6a828fb07eba91af7f7b35a06907
travier <moorlagt@gmail.com>
2019-03-31 12:31:53 -0400

    Remove soft deleted items from getCollection call

--- app/Http/Controllers/API/CollectionController.php
+++ app/Http/Controllers/API/CollectionController.php
@@ -39,7 +39,8 @@ class CollectionController extends Controller
 
         $queryBuilder = DB::table('media')
             ->join('user_media', 'user_media.media_id', '=', 'media.id')
-            ->where('user_media.user_id', $userId);
+            ->where('user_media.user_id', $userId)
+            ->whereNull('user_media.deleted_at');
         
         
         if($randomize) {
