b03b7b287c727cbf0e03a5749f4e6cee52f36748
Travier Moorlag <moorlagt@gmail.com>
2020-05-30 16:07:59 -0400

    Updated frontend libs, app is working well with yarn now

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,27 @@
 import $ from 'jquery';
+import Vue from "vue";
 import { sync } from 'vuex-router-sync';
 import store from './store/index';
 import router from './router';
 import BootstrapVue from 'bootstrap-vue';
 import VueScrollTo from 'vue-scrollto';
 
-window.Vue = require('vue');
+// Page Components
+//@TODO remove and replace
+import About from './pages/about';
+// Components
+import ControlBar from './components/ControlBar'
+import YouTubeCard from './components/YouTubeCard'
+import ThemeButton from './components/ThemeButton'
+import FollowButton from './components/FollowButton'
+import RawDataView from './components/RawDataView'
+import SpotifyAuthorizeButton from './components/SpotifyAuthorizeButton'
+
+// Forms
+import ImportForm from './forms/Import';
+
+
+window.Vue = Vue
 
 require('./bootstrap');
 
@@ -27,20 +43,14 @@ Vue.use(VueScrollTo, {
 });
 
 // Components
-Vue.component('follow-button', require("./components/FollowButton.vue"));
-Vue.component('theme-btn', require("./components/ThemeButton.vue"));
-Vue.component('spotify-authorize-btn', require("./components/SpotifyAuthorizeButton.vue"));
-Vue.component('playlist-media-manager', require('./components/PlaylistMediaManager.vue'));
-//Vue.component('video-player-card', require('./components/VideoPlayerCard.vue'));
-Vue.component('youtube-card', require('./components/YouTubeCard.vue'));
-//Vue.component('master-bar', require('./components/MasterBar.vue'));
-Vue.component('control-bar', require('./components/ControlBar.vue'));
-Vue.component('raw-data-view', require('./components/RawDataView.vue'));
-// Forms
-Vue.component('import-form', require('./forms/Import.vue'));
-
-//Pages
-Vue.component('about-page', require('./pages/about.vue'));
+Vue.component('about-page', About);
+Vue.component('theme-btn', ThemeButton);
+Vue.component('control-bar', ControlBar);
+Vue.component('import-form', ImportForm);
+Vue.component('youtube-card', YouTubeCard);
+Vue.component('raw-data-view', RawDataView);
+Vue.component('follow-button', FollowButton);
+Vue.component('spotify-authorize-btn', SpotifyAuthorizeButton);
 
 router.beforeEach((to, from, next) => {
   $('.navbar-collapse').collapse('hide');
