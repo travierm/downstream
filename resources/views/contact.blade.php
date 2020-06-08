5bdb96fb4671819a79c47192c3e628bc9c075321
Travier Moorlag <moorlagt@gmail.com>
2020-06-08 00:23:55 -0400

    Improved footer layout

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -4,8 +4,7 @@
     <h1 class="pt-5">Contact Us</h1>
     <hr />
     <div class="mt-3">
-        <h3><a class="" href="mailto:downstreamsupport@protonmail.com">Email ( downstreamsupport@protonmail.com )</a></h3>
-        <a target="_blank" href="https://www.facebook.com/DownstreamSocial/"><i style="color: #3b5998;" class="fab fa-facebook fa-5x mt-3"></i></a>
+        <h3>Support Email <a class="" href="mailto:downstreamsupport@protonmail.com"> downstreamsupport@protonmail.com</a></h3>
     </div>
 </div>
 @endsection
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -121,9 +121,9 @@
       <!-- End App -->
       </div>
 
-      <footer class="my-5 pt-5 text-muted text-center text-small ">
+      <footer class=" text-muted text-center text-small">
         <div class="container">
-          <div class="row">
+          <div class="row justify-content-center">
               <div class="col-lg-6">
                   <ul class="list-inline">
                       <li class="list-inline-item"><a class="theme-primary-text" href="/privacy">Privacy</a></li>
@@ -131,11 +131,9 @@
                       <li class="list-inline-item"><a class="theme-primary-text" href="https://www.youtube.com/t/terms">YouTube's Terms of Service</a></li>
                       <li class="list-inline-item"><a class="theme-primary-text" href="/contact">Contact Us</a></li>
                     </ul>
-              </div>
-              <div class="col-lg-6">
-                  <a href="https://youtube.com"><img class="img-fluid" width="300" src="https://developers.google.com/youtube/images/developed-with-youtube-sentence-case-dark.png"/></a>
-                  <p class="mb-1">&copy; {{ date('Y') }} Downstream. All rights reserved.</p>       
-                  <p class="text-muted">Season 1</p>
+                    <p class="mb-1">&copy; {{ date('Y') }} Downstream. All rights reserved.</p>       
+                    <p class="text-muted">Season 1</p>                    
+                    <a href="https://youtube.com"><img class="img-fluid" width="300" src="https://developers.google.com/youtube/images/developed-with-youtube-sentence-case-dark.png"/></a>
               </div>
           </div>
         </div>    
