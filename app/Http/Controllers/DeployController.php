bc571a0bf42b489a22968fe1aec5ed03fbd5c4d5
Travier Moorlag <moorlagt@gmail.com>
2021-01-01 18:21:56 -0500

    Clean up deploy script

--- app/Http/Controllers/DeployController.php
+++ app/Http/Controllers/DeployController.php
@@ -14,21 +14,27 @@ class DeployController extends Controller
 
         // Only deploy on pushes to master
         $requestData = json_decode($request->payload);
-        if($requestData->ref !== 'refs/heads/master') {
+        if ($requestData->ref !== 'refs/heads/master') {
             echo "Only deploying on pushes to master";
             exit;
         }
- 
+
         $localToken = config('app.deploy_secret');
         $localHash = 'sha1=' . hash_hmac('sha1', $githubPayload, $localToken, false);
- 
+
         if (hash_equals($githubHash, $localHash)) {
             $root_path = base_path();
-	    $process = new Process(['./deploy.sh']);
-	    $process->setWorkingDirectory($root_path);
-            $process->run(function ($type, $buffer) {
-                echo $buffer;
+            $process = new Process(['./deploy.sh']);
+            $process->setWorkingDirectory($root_path);
+            $process->start(function ($type, $buffer) {
+                // echo $buffer;
             });
+
+            echo "Deployed script started";
+            exit;
         }
+
+        echo "Failure hash mismatch";
+        exit;
     }
 }
