68cffae131505d9a759956ec9b1b4d9ff6b5885e
Travier Moorlag <moorlagt@gmail.com>
2021-01-02 15:00:45 -0500

    More working on auth routing ( not perfect yet )

--- vue_app/src/router/middleware.js
+++ vue_app/src/router/middleware.js
@@ -1,18 +1,6 @@
 import store from "../store/index"
 
 export function applyMiddleware(router) {
-    // const loggedIn = store.getters["auth/loggedIn"]
-
-    router.beforeEach(async (to, from, next) => {
-        const loggedIn = store.getters["auth/loggedIn"]
-
-        if (!loggedIn) {
-            await store.dispatch("auth/getUser")
-        }
-
-        next()
-    })
-
     // Search Middleware
     router.beforeEach((to, from, next) => {
         if (to.path == "/search" && to.query.query) {
@@ -23,7 +11,7 @@ export function applyMiddleware(router) {
     })
 
     // Redirect to landing page when not loggedIn on requiresAuth routes
-    router.beforeEach((to, from, next) => {
+    router.beforeEach(async(to, from, next) => {
         const loggedIn = store.getters["auth/loggedIn"]
 
         if (to.matched.some((record) => record.meta.requiresAuth)) {
--- vue_app/src/store/modules/auth.js
+++ vue_app/src/store/modules/auth.js
@@ -4,14 +4,14 @@ import AuthService from "@/services/api/AuthService"
 
 export const namespaced = true
 export const state = {
-    user: false,
+    user: window.localStorage.getItem("user"),
     error: false,
     loading: false,
     token: window.localStorage.getItem("token"),
 }
 
 export const mutations = {
-    SET_TOKEN(state, token) {    
+    SET_TOKEN(state, token) {
         state.token = token
         window.localStorage.setItem("token", token)
     },
@@ -23,9 +23,10 @@ export const mutations = {
     },
     SET_USER(state, user) {
         state.user = user
+        window.localStorage.setItem("user", user)
     },
     CLEAR_USER() {
-        state.user = false;
+        state.user = false
         window.localStorage.clear()
     },
 }
@@ -59,15 +60,9 @@ export const actions = {
             return true
         }
 
-        commit("SET_USER", false)
+        commit("CLEAR_USER")
 
-        return AuthService.logout()
-            .then(() => {
-                commit("CLEAR_USER")
-            })
-            .catch(() => {
-                commit("CLEAR_USER")
-            })
+        return true
     },
     getUser({ commit, getters, state }) {
         if (getters.loggedIn) {
