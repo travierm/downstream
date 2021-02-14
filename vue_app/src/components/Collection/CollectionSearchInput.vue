6d4b90ac4188f5df4d2ae7e9de4c3848598acb71
Travier Moorlag <moorlagt@gmail.com>
2021-02-14 11:27:58 -0500

    Fixed collection bar mobile styling

--- vue_app/src/components/Collection/CollectionSearchInput.vue
+++ vue_app/src/components/Collection/CollectionSearchInput.vue
@@ -1,48 +1,50 @@
 <template>
-    <v-text-field
-        color="secondary"
-        v-model="query"
-        solo
-        dense
-        flat
-        label="Search Collection...."
-        clearable
-        hide-details
-        hide-selected
-        light
-    ></v-text-field>
+  <v-text-field
+    color="secondary"
+    v-model="query"
+    solo
+    dense
+    flat
+    label="Search Collection...."
+    clearable
+    hide-details
+    hide-selected
+    light
+  ></v-text-field>
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
+import { mapGetters } from "vuex";
+import YoutubePlayerManager from "../../services/YoutubePlayerManager";
 
 export default {
-    name: "CollectionSearchInput",
-    components: {},
-    computed: {
-        ...mapGetters({
-            collection: "collection/collectionSearchResults",
-        }),
-    },
-    data: () => {
-        return {
-            items: [],
-            query: "",
-            value: "",
-        }
-    },
-    mounted() {},
-    methods: {},
-    watch: {
-        query: function(val) {
-            this.$store.commit('collection/SET_SEARCH_QUERY', val)
+  name: "CollectionSearchInput",
+  computed: {
+    ...mapGetters({
+      collection: "collection/collectionSearchResults",
+    }),
+  },
+  data: () => {
+    return {
+      items: [],
+      query: "",
+      value: "",
+    };
+  },
+  mounted() {},
+  methods: {},
+  watch: {
+    query: function(val) {
+      this.$store.commit("collection/SET_SEARCH_QUERY", val);
 
-            let indexes = _.map(this.collection, "guid")
-            this.$store.dispatch("player/setGuidIndex", indexes)
-            console.log(indexes)
-        },
+      // Stop playing current card so it doesn't get lost when the index is updated
+      YoutubePlayerManager.stopPlayingCard();
+
+      let indexes = _.map(this.collection, "guid");
+      this.$store.dispatch("player/setGuidIndex", indexes);
     },
-}
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
--- vue_app/src/components/views/CollectionView.vue
+++ vue_app/src/components/views/CollectionView.vue
@@ -1,98 +1,74 @@
 <template>
-    <v-container fluid>
-        <v-row>
-            <v-col>
-                <CollectionBar />
-            </v-col>
-        </v-row>
+  <v-container fluid>
+    <v-row>
+      <v-col>
+        <CollectionBar />
+      </v-col>
+    </v-row>
 
-        <v-row v-if="collection == undefined || collection.length <= 0">
-            <v-col>
-                <v-progress-circular
-                    indeterminate
-                    color="primary"
-                ></v-progress-circular>
-            </v-col>
-        </v-row>
+    <v-row v-if="collection == undefined || collection.length <= 0">
+      <v-col>
+        <v-progress-circular
+          indeterminate
+          color="primary"
+        ></v-progress-circular>
+      </v-col>
+    </v-row>
 
-        <v-row v-else>
-            <CardCol v-for="item in collection" :key="item.id">
-                <v-lazy
-                    :options="{
-                        // How many elements should be shown before loading it
-                        threshold: 0.25,
-                    }"
-                    transition="fade-transition"
-                >
-                    <YoutubeCard
-                        :item="item"
-                        :guid="item.guid"
-                        :title="item.title"
-                        :mediaId="item.media_id"
-                        :videoId="item.index"
-                        :thumbnail="item.thumbnail"
-                        :collected="item.collected"
-                    ></YoutubeCard>
-                </v-lazy>
-            </CardCol>
-        </v-row>
+    <v-row v-else>
+      <CardCol v-for="item in collection" :key="item.id">
+        <v-lazy :options="{ threshold: 0.25 }" transition="fade-transition">
+          <YoutubeCard
+            :item="item"
+            :guid="item.guid"
+            :title="item.title"
+            :mediaId="item.media_id"
+            :videoId="item.index"
+            :thumbnail="item.thumbnail"
+            :collected="item.collected"
+          ></YoutubeCard>
+        </v-lazy>
+      </CardCol>
+    </v-row>
 
-        <BottomBar />
-    </v-container>
+    <BottomBar />
+  </v-container>
 </template>
 
 <script>
-import { mapState, mapGetters } from "vuex"
+import { mapGetters } from "vuex";
 
-import CardCol from "@/components/CardCol"
-import BottomBar from "@/components/BottomBar"
-import YoutubeCard from "@/components/YoutubeCard/YoutubeCard"
-import CollectionBar from '@/components/Collection/CollectionBar'
-import CollectionSearchInput from '@/components/Collection/CollectionSearchInput'
+import CardCol from "@/components/CardCol";
+import BottomBar from "@/components/BottomBar";
+import YoutubeCard from "@/components/YoutubeCard/YoutubeCard";
+import CollectionBar from "@/components/Collection/CollectionBar";
 
 export default {
-    name: "CollectionView",
-    components: {
-        CardCol,
-        BottomBar,
-        YoutubeCard,
-        CollectionBar
-    },
-    computed: {
-        ...mapGetters({
-            collection: "collection/collectionSearchResults",
-        }),
-        collectionGuidIndex() {
-            if (!this.collection) {
-                return []
-            }
+  name: "CollectionView",
+  components: {
+    CardCol,
+    BottomBar,
+    YoutubeCard,
+    CollectionBar,
+  },
+  computed: {
+    ...mapGetters({
+      collection: "collection/collectionSearchResults",
+    }),
+    collectionGuidIndex() {
+      if (!this.collection) {
+        return [];
+      }
 
-            return this.collection.map((item) => {
-                return item.guid
-            })
-        },
+      return this.collection.map((item) => {
+        return item.guid;
+      });
     },
-    mounted() {
-        if (this.collectionGuidIndex.length >= 1) {
-            this.$store.dispatch(
-                "player/setGuidIndex",
-                this.collectionGuidIndex
-            )
-        }
-    },
-    watch: {
-        collection(value) {
-            if (this.collectionGuidIndex.length >= 1) {
-                /*this.$store.dispatch(
-                    "player/setGuidIndex",
-                    this.collectionGuidIndex
-                )*/
-            }
-        },
-    },
-    methods: {},
-}
+  },
+  mounted() {
+    if (this.collectionGuidIndex.length >= 1) {
+      this.$store.dispatch("player/setGuidIndex", this.collectionGuidIndex);
+    }
+  },
+};
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped></style>
