aea746eea168bdfcb5a884d5156685de50ebf6f3
Travier Moorlag <moorlagt@gmail.com>
2021-03-07 19:45:53 +0000

    Gitlab CI integration

--- app/Http/Controllers/DeployController.php
+++ app/Http/Controllers/DeployController.php
@@ -14,13 +14,6 @@ class DeployController extends Controller
         $githubPayload = $request->getContent();
         $githubHash = $request->header('X-Hub-Signature');
 
-        // Only deploy on pushes to master
-        $requestData = json_decode($request->payload);
-        if ($requestData->ref !== 'refs/heads/master') {
-            echo "Only deploying on pushes to master";
-            exit;
-        }
-
         $localToken = config('app.deploy_secret');
         $localHash = 'sha1=' . hash_hmac('sha1', $githubPayload, $localToken, false);
 
