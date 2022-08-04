837560adbb4f049a0d804c65dcc78ca11af5e425
Travier Moorlag <moorlagt@gmail.com>
2022-08-03 21:41:25 -0400

    remove old code

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -14,37 +14,4 @@ class UserController extends Controller
     {
         return $req->user();
     }
-
-    public function getConnect(Request $request)
-    {
-        $code = $request->input('code');
-
-        $session = SpotifyAPI::getSession();
-
-        $session->requestAccessToken($code);
-        $accessToken = $session->getAccessToken();
-        $refreshToken = $session->getRefreshToken();
-
-        $userId = Auth::user()->id;
-
-        $token = UserSpotifyToken::where('user_id', $userId);
-        if ($token) {
-            //delete existing tokens
-            $token = UserSpotifyToken::where('user_id', $userId)->delete();
-        }
-
-        $token = new UserSpotifyToken();
-        $token->access_token = $accessToken;
-        $token->refresh_token = $refreshToken;
-        $token->user_id = $userId;
-        $token->save();
-
-        if ($token) {
-            $success = true;
-        } else {
-            $success = false;
-        }
-
-        return redirect('localhost:8000/spotify');
-    }
 }
