0bd2bfc777c944d14c5500b3361944e55e606cab
Travier Moorlag <moorlagt@gmail.com>
2022-08-13 14:51:02 -0400

    Added superdd

--- app/Global/utils.php
+++ app/Global/utils.php
@@ -1,4 +1,14 @@
 <?php
-function dd_json($data) {
-    return response()->json($data, 500);
+function superdd(...$vars)
+{
+	if (!in_array(\PHP_SAPI, ['cli', 'phpdbg'], true) && !headers_sent()) {
+		header('Access-Control-Allow-Origin: *');
+		header('HTTP/1.1 500 Internal Server Error');
+	}
+
+	foreach ($vars as $v) {
+		var_dump($v);
+	}
+
+	exit(1);
 }
