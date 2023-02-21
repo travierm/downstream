50d942e64312dc27fb7ba3eb0613b1586ca31391
Travier Moorlag <moorlagt@gmail.com>
2023-02-20 20:59:12 -0500

    Updated navdrawer

--- vue_app/src/components/views/UserListView.vue
+++ vue_app/src/components/views/UserListView.vue
@@ -2,7 +2,7 @@
   <v-container>
     <v-row justify="center">
       <v-col cols="12">
-        <h1>Downstream Top Users</h1>
+        <h1>Top Users</h1>
       </v-col>
     </v-row>
 
@@ -47,8 +47,4 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-.landingViewContent {
-  text-align: center;
-}
-</style>
+<style scoped></style>
